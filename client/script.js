5083fae47f5428bbdc0235095177caf81de02fe3
Vishal Kumar <21ucc116@lnmiit.ac.in>
2023-01-29 12:37:38 +0530

    Update the service

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://lnm-gpt-20mq.onrender.com/", {
     method: "POST",
     headers: {
       "content-Type": "application/json"
